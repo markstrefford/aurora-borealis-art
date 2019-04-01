37f9d05fb86beb45889bf857fc82f4e04d922108
Mark Strefford <markstrefford@gmail.com>
2019-04-01 22:42:57 +0100

    Saving training output

--- train.py
+++ train.py
@@ -6,10 +6,10 @@ Based on https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
 Repurposed to use Aurora Borealis images instead
 
 """
-from __future__ import print_function
-#%matplotlib inline
+
 import argparse
 import os
+import numpy as np
 import random
 import torch
 import torch.nn as nn
@@ -33,8 +33,8 @@ random.seed(manualSeed)
 torch.manual_seed(manualSeed)
 
 # Root directory for dataset
-# dataroot = 'images/base'
-dataroot = 'images/base/img_align_celeba'
+dataroot = 'images/base'
+# dataroot = 'images/base/img_align_celeba'
 
 # Number of workers for dataloader
 workers = 2
@@ -59,7 +59,7 @@ ngf = 64
 ndf = 64
 
 # Number of training epochs
-num_epochs = 40
+num_epochs = 2
 
 # Learning rate for optimizers
 lr = 0.0002
@@ -281,14 +281,15 @@ for epoch in range(num_epochs):
         iters += 1
 
 # Plot Loss vs training iteration
-plt.figure(figsize=(10,5))
-plt.title("Generator and Discriminator Loss During Training")
-plt.plot(G_losses,label="G")
-plt.plot(D_losses,label="D")
-plt.xlabel("iterations")
-plt.ylabel("Loss")
-plt.legend()
-plt.show()
+# plt.figure(figsize=(10,5))
+# plt.title("Generator and Discriminator Loss During Training")
+# plt.plot(G_losses,label="G")
+# plt.plot(D_losses,label="D")
+# plt.xlabel("iterations")
+# plt.ylabel("Loss")
+# plt.legend()
+# plt.show()
+
 
 # Visualisation of G's progression
 fig = plt.figure(figsize=(8,8))
@@ -302,15 +303,19 @@ HTML(ani.to_jshtml())
 real_batch = next(iter(dataloader))
 
 # Plot the real images
-plt.figure(figsize=(15,15))
-plt.subplot(1,2,1)
-plt.axis("off")
-plt.title("Real Images")
-plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
+# plt.figure(figsize=(15,15))
+# plt.subplot(1,2,1)
+# plt.axis("off")
+# plt.title("Real Images")
+# plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
+#
+# # Plot the fake images from the last epoch
+# plt.subplot(1,2,2)
+# plt.axis("off")
+# plt.title("Fake Images")
+# plt.imshow(np.transpose(img_list[-1],(1,2,0)))
+# plt.show()
 
-# Plot the fake images from the last epoch
-plt.subplot(1,2,2)
-plt.axis("off")
-plt.title("Fake Images")
-plt.imshow(np.transpose(img_list[-1],(1,2,0)))
-plt.show()
+np.save('training_loss_g', G_losses)
+np.save('training_loss_d', D_losses)
+np.save('generated_images', np.transpose(img_list[-1], (1, 2, 0)))
