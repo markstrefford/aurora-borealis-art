eb26852d23daca6b57e0ad1510e35d0d1cbaa8e2
Mark Strefford <markstrefford@gmail.com>
2019-04-01 22:00:04 +0100

    Using celeba to debug GAN

--- train.py
+++ train.py
@@ -33,7 +33,8 @@ random.seed(manualSeed)
 torch.manual_seed(manualSeed)
 
 # Root directory for dataset
-dataroot = 'images/base'
+# dataroot = 'images/base'
+dataroot = 'images/base/img_align_celeba'
 
 # Number of workers for dataloader
 workers = 2
