0c6378d3669be59e7dc71f437919e4fbccfa03fc
Mark Strefford <markstrefford@gmail.com>
2019-04-08 13:55:24 +0100

    dcgan notebook as script

--- train-sagemaker.py
+++ train-sagemaker.py
@@ -28,6 +28,8 @@ from IPython.display import HTML
 # Set random seem for reproducibility
 manualSeed = 999
 #manualSeed = random.randint(1, 10000) # use if you want new results
+
+
 print("Random Seed: ", manualSeed)
 random.seed(manualSeed)
 torch.manual_seed(manualSeed)
