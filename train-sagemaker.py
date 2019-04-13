efc538b2d22e33a535fea4d96049645ec96e1ab1
Mark Strefford <markstrefford@gmail.com>
2019-04-13 17:05:57 +0100

    128x128

--- train-sagemaker.py
+++ train-sagemaker.py
@@ -44,29 +44,33 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            # input is Z, going into a convolution
-            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+            # L1: input is Z, going into a convolution
+            nn.ConvTranspose2d(nz, ngf * 16, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 16),
+            nn.ReLU(True),
+
+            # L2: state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
 
-            # state size. (ngf*8) x 4 x 4
+            # L3: state size. (ngf*8) x 4 x 4
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
 
-            # state size. (ngf*4) x 8 x 8
+            # L4: state size. (ngf*4) x 8 x 8
             nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
 
-            # state size. (ngf*2) x 16 x 16
+            # L5: state size. (ngf*2) x 16 x 16
             nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
 
-            # state size. (ngf) x 32 x 32
-            nn.ConvTranspose2d( ngf, nc, 8, 2, 1, bias=False),
-            # nn.ConvTranspose2d(image_size, nc, 4, 2, 1, bias=False),
+            # L6: state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(image_size, nc, 4, 2, 1, bias=False),
             nn.Tanh()
             # state size. (nc) x 64 x 64
         )
@@ -125,28 +129,32 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            # input is (nc) x 64 x 64
-            # nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
-            nn.Conv2d(nc, image_size, 8, 2, 1, bias=False),
+            # L1: input is (nc) x 64 x 64
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
 
-            # state size. (ndf) x 32 x 32
+            # L2: state size. (ndf) x 32 x 32
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
 
-            # state size. (ndf*2) x 16 x 16
+            # L3: state size. (ndf*2) x 16 x 16
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
 
-            # state size. (ndf*4) x 8 x 8
+            # L4: state size. (ndf*4) x 8 x 8
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
 
-            # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            # L5: state size. (ndf*8) x 8 x 8
+            nn.Conv2d(ndf * 8, ndf * 16, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 16),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # L6: state size. (ndf*8) x 4 x 4
+            nn.Conv2d(ndf * 16, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
         # self.main = nn.Sequential(
@@ -244,7 +252,7 @@ if __name__ =='__main__':
 
     # Spatial size of training images. All images will be resized to this
     #   size using a transformer.
-    image_size = 256 # 64
+    image_size = 128 # 64
 
     # Number of channels in the training images. For color images this is 3
     nc = 3
@@ -253,10 +261,10 @@ if __name__ =='__main__':
     nz = 100
 
     # Size of feature maps in generator
-    ngf = 64  # 128
+    ngf = 128  # 128
 
     # Size of feature maps in discriminator
-    ndf = 64  # 128
+    ndf = 128  # 128
 
     # Number of training epochs
     num_epochs = args.epochs
