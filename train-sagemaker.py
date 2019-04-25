574be60243190b729aeb5865819e143fc293e0b6
EC2 Default User <ec2-user@ip-172-16-5-62.eu-west-1.compute.internal>
2019-04-25 15:41:42 +0000

    Fix to resize of aurora images

--- train-sagemaker.py
+++ train-sagemaker.py
@@ -272,7 +272,7 @@ if __name__ =='__main__':
     print('dataroot={}'.format(dataroot))
     dataset = dset.ImageFolder(root=dataroot,
                                transform=transforms.Compose([
-                                   transforms.Resize(image_size),
+                                   transforms.Resize((image_size, image_size)),
                                    # transforms.CenterCrop(image_size),
                                    transforms.ToTensor(),
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
